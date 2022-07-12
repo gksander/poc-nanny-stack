2d883736e139409bd3d1b9752bec71aa0834678f
Grant Sander <gksander93@gmail.com>
2022-07-12 12:20:20 -0500

    Shared TW config

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,12 +1,7 @@
+const base = require("../../tailwind.config");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  ...base,
   content: ["./**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#F04D21",
-      },
-    },
-  },
-  plugins: [],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F04D21",
+      },
+    },
+  },
+};
